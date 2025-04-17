5cf995e2e1608a9f0b1c64ea676e4ec3e9f47526
Armando Martins <armando.amds97@gmail.com>
2025-04-17 11:30:27 -0300

    realizar as questoes 8 e 9, usando while

---
+++ Revisao10.py
@@ -0,0 +1,14 @@
+#fazer os 2 ultimos exercicios usando while
+#8a questao
+x = 0
+while x<501:
+    if x % 2 != 0:
+        print(x, end=" ")
+    x=x+1
+print("\n")
+#9a questao
+i = 100
+while i>1:
+    if i % 2 == 0:
+        print(i, end = " ")
+    i=i-1
