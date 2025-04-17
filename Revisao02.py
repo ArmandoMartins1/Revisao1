2151c4efcf3690d6c84735b4b7541edcea10cf89
Armando Martins <armando.amds97@gmail.com>
2025-04-17 09:08:42 -0300

    aprimorando o par e impar, adicionando repetição while

--- Revisao02.py
+++ Revisao02.py
@@ -1,13 +1,18 @@
 #Faça um algoritmo para receber um numero qualquer e imprimir na tela se  o numero é par ou impar, positivo ou negativo.
 
-num = int(input("Digite um numero qualquer: "))
+#alterar o arquivo, pondo repetição
+num = 1
 mensagem = " "
-if num % 2 == 0 and num < 0:
-    mensagem = "Par e Negativo"
-elif num % 2 == 0 and num >= 0:
-    mensagem = "Par e Positivo"
-elif num % 2 != 0 and num < 0:
-    mensagem = "Impar e Negativo"
-else:
-    mensagem = "Impar e Positivo"
-print(mensagem)
+while num != 0:
+    num = int(input("Digite um numero qualquer: "))
+    if num % 2 == 0 and num < 0:
+        print("Par e Negativo")
+    elif num % 2 == 0 and num > 0:
+        print("Par e Positivo")
+    elif num % 2 != 0 and num < 0:
+        print("Impar e Negativo")
+    elif num % 2 != 0 and num > 0:
+        print("Impar e Positivo")
+    print("----------------------")
+    print(" DIGITE 0 PARA SAIR ")
+    print("----------------------")
